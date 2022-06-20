5fc7d6e0b7d16f3085bcfc3e3d456f8399b06513
David Henty <d.henty@epcc.ed.ac.uk>
2022-06-20 07:31:23 +0100

    done

---
+++ hello/F-OMP/hello.f90
@@ -0,0 +1,18 @@
+program hello
+
+  use omp_lib
+
+  implicit none
+
+  !$omp parallel
+
+  !$omp master
+  write(*,*) "Running on ", omp_get_num_threads(), " threads"
+  !$omp end master
+
+  write(*,*) "Hello from thread ", omp_get_thread_num()
+
+  !$omp end master
+
+end program hello
+
