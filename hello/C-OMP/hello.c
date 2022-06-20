18e1761e4be694d818ba7618b3b11fa4daba9711
David Henty <d.henty@epcc.ed.ac.uk>
2022-06-20 07:29:15 +0100

    done

---
+++ hello/C-OMP/hello.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main(void)
+{
+  #pragma omp parallel
+  {
+    #pragma omp master
+    {
+      printf("Running in %d threads\n", omp_get_num_threads());
+    }
+
+    printf("Hello from thread %d\n", omp_get_thread_num());
+  }
+
+  return 0;
+}
+  
