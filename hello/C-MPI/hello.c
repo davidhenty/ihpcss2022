85e562b7c5b5a9b3b69488f544c703dd0c00fef4
David Henty <d.henty@epcc.ed.ac.uk>
2022-06-20 06:43:31 +0100

    done

---
+++ hello/C-MPI/hello.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(void)
+{
+  int rank, size;
+  MPI_Comm comm = MPI_COMM_WORLD;
+
+  MPI_Init(NULL,NULL);
+  MPI_Comm_size(comm, &size);
+  MPI_Comm_rank(comm, &rank);
+
+  if (rank == 0) printf("Running on %d process(es)\n", size);
+  printf("Hello from rank %d!\n");
+
+  MPI_Finalize();
+
+  return 0;
+}
